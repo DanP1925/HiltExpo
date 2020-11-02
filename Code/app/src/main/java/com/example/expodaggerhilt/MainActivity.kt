8e7217d9f3c1ea1553ec9c76e154cf337786c018
Daniel Peñaloza <DanP1925@gmail.com>
2020-11-02 17:20:40 -0500

    Constructor Injector

--- Code/app/src/main/java/com/example/expodaggerhilt/MainActivity.kt
+++ Code/app/src/main/java/com/example/expodaggerhilt/MainActivity.kt
@@ -1,7 +1,7 @@
 package com.example.expodaggerhilt
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import dagger.hilt.android.AndroidEntryPoint
@@ -10,21 +10,31 @@ import javax.inject.Inject
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    @Inject
+    lateinit var contactFormatter: ContactFormatter
+
     lateinit var contactAdapter: ContactAdapter
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private lateinit var viewManager: RecyclerView.LayoutManager
-    private val contactData =
-        mutableListOf<String>("Juanito 2934900", "Pablito 12931342", "Maria 128389123")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         viewManager = LinearLayoutManager(this)
-        viewAdapter = ContactAdapter(contactData);
 
-        recyclerView = findViewById<RecyclerView>(R.id.contact_recycler).apply{
+        val contactData =
+            mutableListOf(
+                "Juanito 2934900", "Pablito 12931342", "Maria 128389123"
+            )
+        viewAdapter = ContactAdapter(
+            contactData.map {
+                contactFormatter.giveFormat(it)
+            } as MutableList<String>
+        );
+
+        recyclerView = findViewById<RecyclerView>(R.id.contact_recycler).apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
